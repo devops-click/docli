10fbe27562807549cc9779a47475fa0557abd740
Anderson Ayres <anfergainst@gmail.com>
2024-07-29 13:46:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 #                       docli Installation Script
 ###############################################################################
 
-install_version="0.0.11"
+install_version="0.0.12"
 os_var=$(uname)
 DOCLI_DEPLOY=${DOCLI_DEPLOY:-false}
 DOCLI_REMOTE_REPOSITORY=https://raw.githubusercontent.com/devops-click/docli/main
@@ -240,6 +240,7 @@ declare -a file_paths=(
   "functions/bash_set"
   "functions/bash_terraform"
   "functions/bash_terraform_get_base_files"
+  "functions/runbuild_call"
   "functions/runtf_call"
   "functions/runpacker_call"
   "functions/output_source_files"
